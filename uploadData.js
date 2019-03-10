c11af98e8bf6f99ef41c77a0b59d28c3c4f891a2
ibrahim2311 <i.s.h.94@hotmail.com>
2019-03-10 15:27:54 -0400

    adding functionality to the form button

---
+++ uploadData.js
@@ -0,0 +1,7 @@
+function startDataUpload() {
+    var name = document.getElementById("name").value;
+    var surname = document.getElementById("surname").value;
+    var module = document.getElementById("module").value;
+    var postString = "name="+name +"&surname="+surname+"&module="+module;
+    alert (postString);
+}
